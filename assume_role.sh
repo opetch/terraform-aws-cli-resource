fe19b52c1af0549151ca18eab5d5ae3f64bfa349
Igor Mishchuk <igor.mishchuk@carbonhealth.com>
2023-05-04 16:01:04 +0300

    Update assume_role.sh

--- assume_role.sh
+++ assume_role.sh
@@ -4,19 +4,20 @@ then
   exit 1
 fi
 
-ACCOUNT="$1"
-ROLE="$2"
+# ACCOUNT="$1"
+# ROLE="$2"
 
-role_session_name=`cat /proc/sys/kernel/random/uuid 2>/dev/null || date | cksum | cut -d " " -f 1`
-aws_creds=$(aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT}:role/$ROLE --role-session-name $role_session_name --duration-seconds 3600 --output json)
+# role_session_name=`cat /proc/sys/kernel/random/uuid 2>/dev/null || date | cksum | cut -d " " -f 1`
+# aws_creds=$(aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT}:role/$ROLE --role-session-name $role_session_name --duration-seconds 3600 --output json)
 
-if [ "$?" -ne 0 ]
-then
-  exit 1
-fi
+# if [ "$?" -ne 0 ]
+# then
+#   exit 1
+# fi
 
-export AWS_ACCESS_KEY_ID=$(echo "${aws_creds}" | grep AccessKeyId | awk -F'"' '{print $4}' )
-export AWS_SECRET_ACCESS_KEY=$(echo "${aws_creds}" | grep SecretAccessKey | awk -F'"' '{print $4}' )
-export AWS_SESSION_TOKEN=$(echo "${aws_creds}" | grep SessionToken | awk -F'"' '{print $4}' )
-export AWS_SECURITY_TOKEN=$(echo "${aws_creds}" | grep SessionToken | awk -F'"' '{print $4}' )
-echo "session '$role_session_name' valid for 60 minutes"
+# export AWS_ACCESS_KEY_ID=$(echo "${aws_creds}" | grep AccessKeyId | awk -F'"' '{print $4}' )
+# export AWS_SECRET_ACCESS_KEY=$(echo "${aws_creds}" | grep SecretAccessKey | awk -F'"' '{print $4}' )
+# export AWS_SESSION_TOKEN=$(echo "${aws_creds}" | grep SessionToken | awk -F'"' '{print $4}' )
+# export AWS_SECURITY_TOKEN=$(echo "${aws_creds}" | grep SessionToken | awk -F'"' '{print $4}' )
+# echo "session '$role_session_name' valid for 60 minutes"
+echo "Not needed"
